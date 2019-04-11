d8514144c88d93333553667a23b3c9a4d276b783
Algy <algy@dunamu.com>
2019-04-11 14:39:18 +0900

    Change the default value of compactness_shift to 6

--- fast_slic/fast_slic.py
+++ fast_slic/fast_slic.py
@@ -1,8 +1,8 @@
 from cfast_slic import SlicModel
 
 
-class Slic:
-    def __init__(self, num_components=None, slic_model=None, compactness_shift=5, quantize_level=6):
+class Slic(object):
+    def __init__(self, num_components=None, slic_model=None, compactness_shift=6, quantize_level=6):
         self.compactness_shift = compactness_shift
         self.quantize_level = quantize_level
         self._slic_model = slic_model or SlicModel(num_components or 100)
