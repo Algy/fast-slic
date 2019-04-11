37ad2c6a12a3924f2f552816ee7556b1d8f8a343
Algy <algy@ubuntu.ubuntu-domain>
2019-04-12 05:59:47 +0900

    Cache spatial

--- fast-slic.cpp
+++ fast-slic.cpp
@@ -115,11 +115,14 @@ static void slic_assign_cluster_oriented(Context *context) {
     auto quantize_level = context->quantize_level;
 
     const int16_t S = (int16_t)sqrt(H * W / K);
+    const uint8_t spatial_shift = quantize_level + compactness_shift;
 
+    uint16_t spatial_normalize_cache[2 * S + 2]; // (x) -> (uint16_t)(((uint32_t)x << spatial_shift) / S / 2 * 3) 
+    for (int x = 0; x < 2 * S + 2; x++) {
+        spatial_normalize_cache[x] = (uint16_t)(((uint32_t)x << spatial_shift) / S / 2 * 3);
+    }
 
-    uint8_t spatial_shift = quantize_level + compactness_shift;
-
-     // Sorting clusters by morton order seems to help for distributing clusters evenly for multiple cores
+    // Sorting clusters by morton order seems to help for distributing clusters evenly for multiple cores
     std::vector<const Cluster *> cluster_sorted_ptrs;
     for (int k = 0; k < K; k++) { cluster_sorted_ptrs.push_back(&clusters[k]); }
 
@@ -128,10 +131,10 @@ static void slic_assign_cluster_oriented(Context *context) {
     });
     auto t1 = Clock::now();
 
-    uint16_t spatial_normalize_cache[2 * S + 1]; // (x) -> (uint16_t)(((uint32_t)x << quantize_level) / S)
-    for (int x = 0; x <= 2 * S; x++) {
-        spatial_normalize_cache[x] = (uint16_t)(((uint32_t)x << spatial_shift) / S);
-    }
+    // OPTIMIZATION 1: floating point arithmatics is quantized down to int16_t
+    // OPTIMIZATION 2: L1 norm instead of L2
+    // OPTIMIZATION 5: assignment value is saved combined with distance and cluster number ([distance value (16 bit)] + [cluster number (16 bit)])
+    // OPTIMIZATION 6: Make computations of L1 distance SIMD-friendly
 
     #pragma omp parallel for schedule(static)
     for (int cluster_sorted_idx = 0; cluster_sorted_idx < K; cluster_sorted_idx++) {
@@ -145,52 +148,6 @@ static void slic_assign_cluster_oriented(Context *context) {
         const int16_t local_buffer_height = y_hi - y_lo;
         uint16_t* local_buffer = new uint16_t[local_buffer_height * local_buffer_width];
 
-        // OPTIMIZATION 1: floating point arithmatics is quantized down to int16_t
-        // OPTIMIZATION 2: L1 norm instead of L2
-        // OPTIMIZATION 3: L1 normalizer(x / 3) ommitted in the color distance term
-        // OPTIMIZATION 4: L1 normalizer(x / 2) ommitted in the spatial distance term
-        // OPTIMIZATION 5: assignment value is saved combined with distance and cluster number ([distance value (16 bit)] + [cluster number (16 bit)])
-        // OPTIMIZATION 6: Make computations of L1 distance SIMD-friendly
-
-        // Calculate Spatial Distances (not quantized or normalized yet)
-        // 4 3 2 3 4
-        // 3 2 1 2 3
-        // 2 1 0 1 2
-        // 3 2 1 2 3
-        // 4 3 2 3 4
-        // Build the first row
-        {
-            uint16_t first_row_value = (cluster_y - y_lo) + (cluster_x - x_lo);
-            for (int16_t t = 0; t <= (cluster_x - x_lo); t++) {
-                local_buffer[t] = first_row_value--;
-            }
-            first_row_value++; // recover value that decreased by 1 one more
-            for (int16_t t = (cluster_x - x_lo) + 1; t < local_buffer_width; t++) {
-                local_buffer[t] = ++first_row_value;
-            }
-        }
-
-        // Fill in values up to half rows
-        for (int16_t bi = 1; bi <= (cluster_y - y_lo); bi++) {
-            for (int16_t bj = 0; bj < local_buffer_width; bj++) {
-                local_buffer[local_buffer_width * bi + bj] = local_buffer[local_buffer_width * (bi - 1) + bj] - 1;
-            }
-        }
-
-        // Fill in other half rows
-        for (int16_t bi = (cluster_y - y_lo) + 1; bi < local_buffer_height; bi++) {
-            for (int16_t bj = 0; bj < local_buffer_width; bj++) {
-                local_buffer[local_buffer_width * bi + bj] = local_buffer[local_buffer_width * (bi - 1) + bj] + 1;
-            }
-        }
-
-        // quantize and normalize
-        for (int16_t bi = 0; bi < local_buffer_height; bi++) {
-            for (int16_t bj = 0; bj < local_buffer_width; bj++) {
-                local_buffer[local_buffer_width * bi + bj] = spatial_normalize_cache[local_buffer[local_buffer_width * bi + bj]];
-            }
-        }
-
         // spatial distances are done in local_buffer
         // Now, add color distances
         for (int16_t bi = 0; bi < local_buffer_height; bi++) {
@@ -199,7 +156,8 @@ static void slic_assign_cluster_oriented(Context *context) {
                 int32_t img_base_index = 3 * (W * i + j);
                 uint8_t r = image[img_base_index], g = image[img_base_index + 1], b = image[img_base_index + 2];
                 uint16_t color_dist = ((uint32_t)(fast_abs<int16_t>(r - (int16_t)cluster->r) + fast_abs<int16_t>(g - (int16_t)cluster->g) + fast_abs<int16_t>(b - (int16_t)cluster->b)) << quantize_level);
-                local_buffer[local_buffer_width * bi + bj] += color_dist;
+                uint16_t spatial_dist = spatial_normalize_cache[fast_abs<int16_t>(i - cluster_y) + fast_abs<int16_t>(j - cluster_x)];
+                local_buffer[local_buffer_width * bi + bj] = spatial_dist + color_dist;
             }
         }
 
