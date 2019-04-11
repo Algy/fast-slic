00ab0fe9c42e60d3382bc6f7ce556ec8782feabb
Algy <algy@dunamu.com>
2019-04-11 15:23:04 +0900

    Release 0.1.0 was not successful. Fix it

--- setup.py
+++ setup.py
@@ -1,27 +1,36 @@
 #!/usr/bin/env python
 
 import os
-import numpy as np
 import platform
 
 from setuptools import find_packages
+from setuptools.command.build_ext import build_ext as _build_ext
 from distutils.core import setup
 from distutils.extension import Extension
-from Cython.Build import cythonize
 
 extra_compile_args = []
 if platform.system() != 'Windows':
     extra_compile_args += ['-std=c++11']
 
+class build_ext(_build_ext):
+    def finalize_options(self):
+        _build_ext.finalize_options(self)
+        # Prevent numpy from thinking it is still in its setup process:
+        __builtins__.__NUMPY_SETUP__ = False
+        import numpy
+        self.include_dirs.append(numpy.get_include())
+
+
+
 
 setup(
     name="fast-slic",
-    version="0.1.0",
+    version="0.1.1",
     description="Fast Slic Superpixel Implementation",
     author="Alchan Kim",
     author_email="a9413miky@gmail.com",
-    setup_requires = ["cython", "numpy"],
-    install_requires=["numpy"],
+    setup_requires = ["setuptools>=18.0", "cython", "numpy>=1.8"],
+    install_requires=["numpy>=1.8"],
     python_requires=">=3.5",
     license="MIT",
     classifiers=[
@@ -33,20 +42,19 @@ setup(
         "Programming Language :: Python :: Implementation :: CPython",
     ],
     packages=find_packages(),
-    ext_modules=cythonize(
-        [
-            Extension(
-                "libfastslic",
-                sources=["fast-slic.cpp"],
-                extra_compile_args=extra_compile_args,
-                language='c++11',
-            ),
-            Extension(
-                "cfast_slic",
-                sources=["cfast_slic.pyx"],
-                include_dirs=[np.get_include()],
-            )
-        ],
-    )
+    package_data={'': ['*.pyx', '*.pxd', '*.h', '*.cpp']},
+    cmdclass={'build_ext':build_ext},
+    ext_modules=[
+        Extension(
+            "libfastslic",
+            sources=["fast-slic.cpp"],
+            extra_compile_args=extra_compile_args,
+            language='c++11',
+        ),
+        Extension(
+            "cfast_slic",
+            sources=["cfast_slic.pyx"],
+        )
+    ],
 )
 
