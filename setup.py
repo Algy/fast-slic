8eef3b003f72be19c942e625c222340d199ab3e5
Algy <algy@ubuntu.ubuntu-domain>
2019-04-27 03:45:29 +0900

    Release 0.3.0

--- setup.py
+++ setup.py
@@ -79,7 +79,7 @@ else:
 
 setup(
     name="fast-slic",
-    version="0.2.5",
+    version="0.3.0",
     description="Fast Slic Superpixel Implementation",
     author="Alchan Kim",
     author_email="a9413miky@gmail.com",
